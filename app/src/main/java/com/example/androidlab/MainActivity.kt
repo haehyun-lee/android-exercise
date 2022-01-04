e5fda6ee1c595b14ffb6c9b0a6155a680ef26afb
bada <bada7144@gmail.com>
2022-01-04 18:46:11 +0900

    Add ch9_resource

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,33 +1,13 @@
 package com.example.androidlab
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.checkbox.setOnCheckedChangeListener(object: CompoundButton.OnCheckedChangeListener {
-            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
-                Log.d("kkang", "체크박스 클릭")
-            }
-        })
-
-        binding.button.setOnClickListener {
-            Log.d("kkang", "클릭 이벤트")
-        }
-
-        binding.button.setOnLongClickListener {
-            Log.d("kkang", "롱클릭 이벤트")
-            true
-        }
+        setContentView(R.layout.activity_main)
     }
 
 }
