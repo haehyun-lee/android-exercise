81c6feb48b485c215e6137329be6b897c293ec5b
bada <bada7144@gmail.com>
2022-01-09 15:14:03 +0900

    Add ch11 recyclerview

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.androidlab
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidlab.databinding.ActivityRecyclerviewBinding
@@ -29,7 +37,10 @@ class MainActivity : AppCompatActivity() {
         // RecyclerView 설정
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = MyAdapter(datas)
-        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        binding.recyclerView.addItemDecoration(MyDecoration(this))
+
+        datas.add("new data")
+        (binding.recyclerView.adapter as MyAdapter).notifyDataSetChanged()
    }
 }
 
@@ -60,5 +71,63 @@ class MyAdapter(val datas: MutableList<String>) : RecyclerView.Adapter<RecyclerV
     // 총 데이터 개수 반환
     override fun getItemCount(): Int {
         return datas.size
+    }
+}
+
+// 사용자 정의 ItemDecoration
+class MyDecoration(val context: Context) : RecyclerView.ItemDecoration() {
+    // 항목 아래 그려질 이미지
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+        c.drawBitmap(
+            BitmapFactory.decodeResource(context.resources, R.drawable.big),
+            0f,
+            0f,
+            null)
+    }
+
+    // 항목 위에 그려질 이미지
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+        // 뷰 크기 계산
+        val width = parent.width
+        val height = parent.height
+
+        // 이미지 크기 계산
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        val drWidth = dr?.intrinsicWidth
+        val drHeight = dr?.intrinsicHeight
+
+        // 이미지가 그려질 위치 계산
+        val left = width / 2 - drWidth?.div(2) as Int       // 뷰 정가운데 위치에서 그림 절반만큼 왼쪽으로 이동
+        val top = height / 2 - drHeight?.div(2) as Int
+        c.drawBitmap(
+            BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            left.toFloat(),
+            top.toFloat(),
+            null
+        )
+    }
+
+    // 개별 항목 데코
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view) + 1
+
+        // 3의 배수 번째 항목에선 아래 여백을 크게 설정
+        if (index % 3 == 0) {
+            outRect.set(10, 10, 10, 60)     // 각 항목 표시 사각형 영역 정보
+        } else {
+            outRect.set(10, 10, 10, 0)
+        }
+
+        // 색상 지정
+        view.setBackgroundColor(Color.LTGRAY)
+        ViewCompat.setElevation(view, 20.0f)
     }
 }
