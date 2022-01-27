bb2c0aebc1672366bdef5318cf0cb2b545463011
bada <bada7144@gmail.com>
2022-01-27 21:13:26 +0900

    Add ch16 contacts

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,40 +1,32 @@
 package com.example.androidlab
 
-import android.app.job.JobInfo
-import android.app.job.JobScheduler
-import android.content.ComponentName
-import android.os.Build
+import android.content.Intent
 import android.os.Bundle
-import android.os.Messenger
-import android.os.PersistableBundle
-import androidx.annotation.RequiresApi
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.getSystemService
 import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var messenger: Messenger
+    lateinit var binding: ActivityMainBinding
+    lateinit var requestContactsLauncher: ActivityResultLauncher<Intent>
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var jobScheduler: JobScheduler? = getSystemService<JobScheduler>()
-        val extras = PersistableBundle()
-        extras.putString("extra_data", "hello world")
-        val builder = JobInfo.Builder(1, componentName)
-        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)  // 네트워크 타입
-        builder.setRequiresCharging(true)   // 배터리 충전 상태
-        builder.setExtras(extras)
-        val jobInfo = builder.build()
-        jobScheduler!!.schedule(jobInfo)
+        requestContactsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == RESULT_OK) {
+                Log.d("kkang", "${it.data?.data}")
+            }
+        }
 
-
-        JobInfo.Builder(1, ComponentName(this, MyJobService::class.java)).run {
-            setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-            jobScheduler?.schedule(build())
+        binding.button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            requestContactsLauncher.launch(intent)
         }
 
     }
--- ch16_provider/src/main/java/com/example/ch16_provider/MainActivity.kt
+++ ch16_provider/src/main/java/com/example/ch16_provider/MainActivity.kt
@@ -1,55 +1,78 @@
 package com.example.ch16_provider
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.ch16_provider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var getContent: ActivityResultLauncher<Intent>
+    lateinit var requestLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 퍼미션 허용했는지 확인
+        val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
+        if (status == PackageManager.PERMISSION_GRANTED) {
+            Log.d("test", "permission granted")
+        } else {
+            // 퍼미션 요청 다이얼로그 표시
+            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.READ_CONTACTS"), 100)
+            Log.d("test", "permission denied")
+        }
+
         // ActivityResultLauncher 초기화, 결과 콜백 정의
-        getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
-                // 시스템의 콘텐츠 프로바이더 사용
+                Log.d("test", "Uri : ${it.data!!.data!!}")
                 val cursor = contentResolver.query(
                     it.data!!.data!!,
                     arrayOf<String>(
                         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                        ContactsContract.CommonDataKinds.Phone.NUMBER,
                     ),
                     null,
                     null,
                     null
                 )
-                Log.d("test", "ActivityResult.Intent.data : ${it.data!!.data!!}")
                 Log.d("test", "cursor size : ${cursor?.count}")
-                if (cursor!!.moveToFirst()) {
-                    val name = cursor?.getString(0)
-                    val phone = cursor?.getString(1)
-                    Log.d("test", "name : $name, phone : $phone")
-                }
 
-//                val result: String? = it.data?.dataString
-//                Log.d("result", "result : $result")
+                if (cursor!!.moveToFirst()) {
+                    val name = cursor.getString(0)
+                    val phone = cursor.getString(1)
+                    binding.textView.text = "name: $name, phone: $phone"
+                }
             }
         }
 
         binding.button.setOnClickListener {
             // 주소록 앱 연동
             val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
-            Log.d("test", "Uri : ${intent.data}")
-            getContent.launch(intent)
+            requestLauncher.launch(intent)
+        }
+    }
+
+    // 다이얼로그에서 퍼미션 허용했는지 확인
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d("test", "permission granted")
+        } else {
+            Log.d("test", "permission denied")
         }
     }
 }
