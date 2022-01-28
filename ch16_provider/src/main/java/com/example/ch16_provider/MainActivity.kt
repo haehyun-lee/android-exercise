6d2eb7d612e111ed37fb41a9e4584cd1b4ba84b6
bada <bada7144@gmail.com>
2022-01-28 21:38:00 +0900

    Add ch16 gallery

--- ch16_provider/src/main/java/com/example/ch16_provider/MainActivity.kt
+++ ch16_provider/src/main/java/com/example/ch16_provider/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.ch16_provider
 
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.ch16_provider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,48 +20,78 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 퍼미션 허용했는지 확인
+        /*
         val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
         if (status == PackageManager.PERMISSION_GRANTED) {
             Log.d("test", "permission granted")
         } else {
-            // 퍼미션 요청 다이얼로그 표시
             ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.READ_CONTACTS"), 100)
             Log.d("test", "permission denied")
         }
+        */
 
-        // ActivityResultLauncher 초기화, 결과 콜백 정의
         requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK) {
-                Log.d("test", "Uri : ${it.data!!.data!!}")
-                val cursor = contentResolver.query(
-                    it.data!!.data!!,
-                    arrayOf<String>(
-                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                        ContactsContract.CommonDataKinds.Phone.NUMBER,
-                    ),
-                    null,
-                    null,
-                    null
-                )
-                Log.d("test", "cursor size : ${cursor?.count}")
+            try {
+                // 출력할 이미지의 화면 크기
+                val calRatio = calculateInSampleSize(it!!.data!!.data!!, 300, 300)
+                val option = BitmapFactory.Options()
+                option.inSampleSize = calRatio
 
-                if (cursor!!.moveToFirst()) {
-                    val name = cursor.getString(0)
-                    val phone = cursor.getString(1)
-                    binding.textView.text = "name: $name, phone: $phone"
+                // 이미지 불러오기
+                var inputStream = contentResolver.openInputStream(it!!.data!!.data!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
+                inputStream!!.close()
+                inputStream = null
+                bitmap?.let {
+                    binding.galleryResult.setImageBitmap(bitmap)
+                }?: let{
+                    Log.d("kkang", "bitmap null")
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
 
+
         binding.button.setOnClickListener {
-            // 주소록 앱 연동
-            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            // 갤러리 앱 실행
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
             requestLauncher.launch(intent)
         }
     }
 
-    // 다이얼로그에서 퍼미션 허용했는지 확인
+    private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int {
+        val options = BitmapFactory.Options()
+        // Bitmap 객체는 생성 안하고 옵션만 설정할 것
+        options.inJustDecodeBounds = true
+        try {
+            // 콘텐츠 프로바이더 사용
+            var inputStream = contentResolver.openInputStream(fileUri)
+            // 각종 이미지 정보가 option 객체에 설정
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream!!.close()
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        // 이미지 크기 정보 가져오기
+        val(height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        // inSampleSize 계산
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
+    }
+
+    /*
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -75,4 +104,6 @@ class MainActivity : AppCompatActivity() {
             Log.d("test", "permission denied")
         }
     }
+
+     */
 }
